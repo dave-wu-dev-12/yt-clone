9b32594ce0f037d50a04ec2d5d53d530fac23662
d wu <dwu@ds-MacBook-Pro.local>
2020-08-16 17:34:29 -0500

    adding recommended vids

---
+++ src/Video.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Video.css";
+import Avatar from "@material-ui/core/Avatar";
+
+function Video({ thumbnail, avatar, title, channel, views, timestamp }) {
+  return (
+    <div className="videoCard">
+      <img src={thumbnail} alt="thumbnail" className="videoCard__thumbnail" />
+      <div className="videoCard__info">
+        <Avatar className="videoCard__thumb" src={avatar} alt="avatar" />
+        <div className="videoCard__details">
+          <h4 className="videCard__vidName">{title}</h4>
+          <p className="videoCard__channel">{channel}</p>
+          <p class="videoCard__views">
+            {views}
+            &nbsp; &#8226; &nbsp;
+            {timestamp}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Video;
